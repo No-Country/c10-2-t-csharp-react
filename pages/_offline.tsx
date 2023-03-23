cc8a1cdf030129aa695a0aa04b92311c414709bc
MarcosSWeis <marcosweis01@gmail.com>
2023-03-23 16:47:23 -0300

    Add structure folders

--- pages/_offline.tsx
+++
@@ -1,3 +0,0 @@
-export default function Offline() {
-  return <h1>This is offline fallback page</h1>
-}
--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    'postcss-easy-import',
-    'tailwindcss/nesting',
-    'tailwindcss',
-    'autoprefixer',
-  ]
-}
